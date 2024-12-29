188bb43161da3a3432ce8c4fe5ac2caaea476c78
MandlH <harald.mandl@outlook.com>
2024-12-29 17:43:37 +0100

    Remove test code

--- BasicBanking/src/main/java/org/mandl/Main.java
+++ BasicBanking/src/main/java/org/mandl/Main.java
@@ -8,24 +8,5 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        IdentityUserRepository userRepository = new IdentityUserRepository();
-        try {
-            // Save a new user
-            userRepository.save(IdentityUser.create("TestUser2", "TestPassword"));
-
-            // Fetch all users
-            List<IdentityUser> users = userRepository.findAll();
-            if (!users.isEmpty()) {
-                for (IdentityUser user : users) {
-                    System.out.println(user.getUsername());
-                }
-            } else {
-                System.out.println("No users found.");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            userRepository.close();
-        }
     }
 }
