0aa007acba94cdfdb12de35cd947c00da36a6f8b
MandlH <harald.mandl@outlook.com>
2024-12-30 16:11:33 +0100

    Add MessageHandler

--- BasicBanking/Utilities/src/main/java/org/mandl/exceptions/ExceptionHandler.java
+++ BasicBanking/Utilities/src/main/java/org/mandl/exceptions/ExceptionHandler.java
@@ -1,28 +1,18 @@
 package org.mandl.exceptions;
 
-import org.mandl.LoggingHandler;
+import org.mandl.message.MessageHandler;
 
 import java.util.Scanner;
 
 public class ExceptionHandler {
-    private static final LoggingHandler logger = LoggingHandler.getLogger(ExceptionHandler.class);
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static void handleException(Exception e) {
         if (e instanceof AuthenticationException) {
-            System.out.println("-".repeat(7 + e.getMessage().length()));
-            System.out.println("Error: " + e.getMessage());
-            System.out.println("Press Enter to continue...");
-            System.out.println("-".repeat(7 + e.getMessage().length()));
-            scanner.nextLine();
-            logger.info(e.getMessage());
+            MessageHandler.displayError(e.getMessage());
         } else {
-            System.out.println("-".repeat(45));
-            System.out.println("Error: An unexpected error occurred");
-            System.out.println("Press Enter to continue...");
-            System.out.println("-".repeat(45));
-            scanner.nextLine();
-            logger.error(e.getMessage());
+            MessageHandler.displayError("An unexpected error occurred", e);
         }
     }
 }
---
+++ BasicBanking/Utilities/src/main/java/org/mandl/message/MessageHandler.java
@@ -0,0 +1,35 @@
+package org.mandl.message;
+
+import org.mandl.LoggingHandler;
+import org.mandl.exceptions.ExceptionHandler;
+
+import java.util.Scanner;
+
+public class MessageHandler {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final LoggingHandler logger = LoggingHandler.getLogger(ExceptionHandler.class);
+
+    public static void displayMessage(String message) {
+        int length = 7 + message.length();
+        System.out.println("-".repeat(length));
+        System.out.println("Message: " + message);
+        System.out.println("Press Enter to continue...");
+        System.out.println("-".repeat(length));
+        scanner.nextLine();
+        logger.info(message);
+    }
+
+    public static void displayError(String errorMessage) {
+        int length = 7 + errorMessage.length();
+        System.out.println("-".repeat(length));
+        System.out.println("Error: " + errorMessage);
+        System.out.println("Press Enter to continue...");
+        System.out.println("-".repeat(length));
+        scanner.nextLine();
+    }
+
+    public static void displayError(String errorMessage, Exception e) {
+        displayError(errorMessage);
+        logger.error(e.getMessage());
+    }
+}
